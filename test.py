2f6bdbd21a5b02c40dbaac88777fa83d7e4759c9
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-18 11:52:24 +0530

    change

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 import time
 
-
+def automate(driver):
+  
 
 if __name__ == "__main__":
-  browser = webdriver.Chrome()
-  browser.get('https://stackoverflow.com/users/login')
+  browser = webdriver.Chrome('/Users/acephoenix02/Google Drive/Documents/Python/Online-Class-Automation/chromedriver')
+  automate(browser)
   time.sleep(10)
