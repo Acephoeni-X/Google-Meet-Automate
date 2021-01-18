ac67306500058fb0b9557fb6c9454da7613138e4
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-18 13:34:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,16 +7,17 @@ import schedule
 import time
 
 def automate(driver, link):
-  #driver.get(link)
+  driver.get(link)
   #driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[3]/div[2]/div/div/span/span/div/div/svg').click()
   # wait = WebDriverWait(driver, 20)
   # wait.until(driver.get(link))
   # element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]')))
   # element.send_keys("rishi.d.goku")
   driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys("rishi.d.goku")
+  driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
 
 def schedule_time_table(browser):
-  schedule.every().monday.at("13:21").do(automate, browser, 'https://accounts.google.com/ServiceLogin/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2F&followup=https%3A%2F%2Fclassroom.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=AddSession')
+  schedule.every().monday.at("13:25").do(automate, browser, 'https://accounts.google.com/ServiceLogin/identifier?service=classroom&passive=1209600&continue=https%3A%2F%2Fclassroom.google.com%2F&followup=https%3A%2F%2Fclassroom.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=AddSession')
 
   while True:
     schedule.run_pending()
