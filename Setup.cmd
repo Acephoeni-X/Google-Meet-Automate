b2347c8e56e8b1a3dfd0e224b1e26c8344c0d119
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-28 16:30:26 +0530

    change

--- Setup.cmd
+++ Setup.cmd
@@ -1,9 +1,9 @@
 @echo off
+echo "Script will wait 3 second to check additional functionalities"
+timeout 3 >nul
 cd Script
-Test.bat
-python3 -m pip install –upgrade pip
-cls
-python3 -m pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 cls
 python3 telegram.py
 echo "Script is running do not close the application"
+timeout 5 >nul
